8f6f4d64f0f62199aa3db28b603c1eaa141803db
Pedro Marques <83501969+PedroMarquesr@users.noreply.github.com>
2024-12-16 10:53:27 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var leonardo = window.document.getElementById("leonardo");
-var samantha = window.document.getElementById("samantha");
-var bruna = window.document.getElementById("bruna");
-var seta_direita = window.document.getElementById("seta-direita");
-var seta_esquerda = window.document.getElementById("seta-esquerda");
+var seta_direita = window.document.getElementById("seta-direita")
+var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById("samantha")
+var bruna = window.document.getElementById("bruna")
+var seta_esquerda = window.document.getElementById("seta-esquerda")
 
 function rolar_a_direita() {
-  leonardo.style = "display:none";
-  bruna.style = "display:flex";
-  seta_esquerda.style = "display:flex";
-  seta_direita.style = "display:none";
+  leonardo.style = "display:none"
+  bruna.style = "display:flex"
+  seta_esquerda.style = "display:flex; margin-top:55px"
+  seta_direita.style = "display:none"
 }
 
 function rolar_a_esquerda() {
-  leonardo.style = "display:flex";
-  bruna.style = "display:none";
-  seta_esquerda.style = "display:none";
-  seta_direita.style = "display:flex";
+  leonardo.style = "display:flex"
+  bruna.style = "display:none"
+  seta_direita.style = "display:flex; margin-top:55px"
+  seta_esquerda.style = "display:none"
 }
